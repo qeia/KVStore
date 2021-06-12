47ca78fb3a6be8dd6748765cce2d7a7482854aed
qeia <ffifasiddharth@gmail.com>
2021-06-12 16:35:17 +0530

    added replica (#2)
    Co-authored-by: Siddharth Iyer <siddharth.bpgc@gmail.com>

--- src/main/java/com/sid/KVStore/Value.java
+++ src/main/java/com/sid/KVStore/Value.java
@@ -1,12 +1,46 @@
 package com.sid.KVStore;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.inject.Singleton;
+import lombok.*;
 import org.joda.time.DateTime;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Value {
-    private DateTime dateTime;
+
+    @JsonProperty
+    private Long dateTime;
+    @JsonProperty
     private String value;
+
+    public Value (String value){
+        this.value = value;
+        this.dateTime = new DateTime().getMillis();
+    }
+
+    public Value (String value, Long dateTime){
+        this.value = value;
+        this.dateTime = dateTime;
+    }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Value value1 = (Value) o;
+        return value.equals(value1.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
